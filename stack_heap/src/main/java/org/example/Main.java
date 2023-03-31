b5e4ca666d0e9339e7f8ba0cce0284203aec4399
ismail harik <92827404+Ismailharik@users.noreply.github.com>
2023-03-31 18:06:46 +0000

    add Stack ,Heap, Generics

---
+++ stack_heap/src/main/java/org/example/Main.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import org.example.generics.GenericQueue;
+import org.example.generics.GenericStack;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--------------- QUEUE -----------------------");
+
+        QueueOfString queue = new QueueOfString();
+        queue.enqueue("1");
+        queue.enqueue("2");
+        queue.enqueue("3");
+        System.out.println(queue.deQueue());
+        System.out.println(queue.deQueue());
+        System.out.println(queue.deQueue());
+
+        System.out.println("--------------- STACK -----------------------");
+
+        StackOfString stack = new StackOfString();
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+
+        System.out.println("---------------GENERIC QUEUE -----------------------");
+
+        GenericQueue genericQueue = new GenericQueue();
+        genericQueue.enqueue(1);
+        genericQueue.enqueue(2);
+        genericQueue.enqueue(3);
+        System.out.println(genericQueue.deQueue());
+        System.out.println(genericQueue.deQueue());
+        System.out.println(genericQueue.deQueue());
+
+
+        System.out.println("---------------GENERIC STACK -----------------------");
+
+        GenericStack<Integer> genericStack = new GenericStack<Integer>();
+        genericStack.push(1);
+        genericStack.push(2);
+        genericStack.push(3);
+        System.out.println(genericStack.pop());
+        System.out.println(genericStack.pop());
+        System.out.println(genericStack.pop());
+    }
+
+}
---
+++ stack_heap/src/main/java/org/example/generics/GenericQueue.java
@@ -0,0 +1,30 @@
+package org.example.generics;
+
+public class GenericQueue<Item> {
+        private Node first,last;
+    class Node {
+        Item data;
+        Node next=null;
+        public Node(Item item){
+            this.data = item;
+        }
+    }
+    public void enqueue(Item item){
+        Node newNode = new Node(item);
+       if(isEmpty()){
+           first=last=newNode;
+       }else{
+           last.next=newNode;
+           last = newNode;
+       }
+    }
+    public Item deQueue(){
+        Item item=first.data;
+        first = first.next;
+        return item;
+    }
+    public boolean isEmpty(){
+            return null==this.first;
+    }
+
+}
